c41685b1f03f4ae2ad6067db3c552289c993694c
Feitsma <he.feitsma@gmail.com>
2019-11-26 23:08:06 +0100

    Tried to create a Class with a function that determines the distance between two points. No success in the form of a class, a simple functions forms no problem.

--- Chapter 7/chapter7.py
+++ Chapter 7/chapter7.py
@@ -1,2 +1,27 @@
-"this is a test file to test GitHub"
+"Exercise 11.1.12"
 
+import math
+
+def distance(x1, y1, x2, y2):
+    return math.sqrt((x2-x1)**2 + (y2-y1)**2)
+
+class Point:
+    """Point class represents and manipulates x,y coords."""
+
+    def __init__(self, x=0, y=0):
+        """Create a new point at x,y"""
+        self.x = x
+        self.y = y
+
+    def distance_class(self):
+        """Calculates distance from each other"""
+        x_dist = q.x - p.x
+        y_dist = q.y - p.y
+        return math.sqrt((x_dist)**2 + (y_dist)**2)
+
+p = Point(3,4)
+q = Point(5,4)
+
+
+print(p.x, p.y, q.x, q.y)
+to_Print = distance_class(p,q)
