04dab213775dcc5683be7fd6b822aa4267697879
Feitsma <he.feitsma@gmail.com>
2019-11-30 19:50:06 +0100

    Removed distance function, was not working

--- Chapter 7/chapter7.py
+++ Chapter 7/chapter7.py
@@ -2,9 +2,6 @@
 
 import math
 
-def distance(x1, y1, x2, y2):
-    return math.sqrt((x2-x1)**2 + (y2-y1)**2)
-
 class Point:
     """Point class represents and manipulates x,y coords."""
 
@@ -13,15 +10,8 @@ class Point:
         self.x = x
         self.y = y
 
-    def distance_class(self):
-        """Calculates distance from each other"""
-        x_dist = q.x - p.x
-        y_dist = q.y - p.y
-        return math.sqrt((x_dist)**2 + (y_dist)**2)
-
 p = Point(3,4)
 q = Point(5,4)
 
 
 print(p.x, p.y, q.x, q.y)
-to_Print = distance_class(p,q)
