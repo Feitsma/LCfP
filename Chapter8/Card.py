5be020c4dac8fbaac86bd2dc7e6736037b1785ca
Feitsma <he.feitsma@gmail.com>
2019-12-05 18:19:37 +0100

    Finished

--- Chapter8/Card.py
+++ Chapter8/Card.py
@@ -38,4 +38,42 @@ class Card:
     def __ne__(self, other):
         return self.cmp(other) != 0
 
+class Deck:
+    def __init__(self):
+        self.cards = []
+        for suit in range(4):
+            for rank in range(1,14):
+                self.cards.append(Card(suit, rank))
 
+    def print_deck(self):
+        for card in self.cards:
+            print(card)
+
+    def __str__(self):
+        s = " "
+        for i in range (len(self.cards)):
+            s = s + " " * i + str(self.cards[i]) + "\n"
+        return s
+
+    def shuffle(self):
+        import random
+        rng = random.Random()
+        rng.shuffle(self.cards)
+
+    def remove(self, card):
+        if card in self.cards:
+            self.cards.remove(card)
+            return True
+        else:
+            return False
+
+    def pop(self):
+        #pop function removes last card in list and returns it
+        return self.cards.pop()
+
+    def is_empty(self):
+        return self.cards == []
+
+#Create a deck
+red_deck = Deck()
+blue_deck = Deck()
